2eeb2a6241c97ce022930add9249b00a66f1fcb6
lowe21 <78665806@qq.com>
2026-08-05 11:53:39 +0800

    导入核心初始化

--- core/core.go
+++ core/core.go
@@ -2,8 +2,16 @@ package core
 
 import (
 	"github.com/lowe21/lxv/core/config_center"
+	"github.com/lowe21/lxv/core/log_center"
+	"github.com/lowe21/lxv/core/orm"
+	"github.com/lowe21/lxv/core/swagger"
+	"github.com/lowe21/lxv/core/trace"
 )
 
 func init() {
 	config_center.Init()
+	log_center.Init()
+	orm.Init()
+	swagger.Init()
+	trace.Init()
 }
