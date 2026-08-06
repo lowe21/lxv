cad637984c2c8df4ca28990a8407f35d90b98dc5
lowe21 <78665806@qq.com>
2026-08-06 15:52:22 +0800

    增加定时任务

---
+++ pkg/crontab/crontab.go
@@ -0,0 +1,122 @@
+package crontab
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/olekukonko/tablewriter/tw"
+
+	"github.com/gogf/gf/v2/container/garray"
+	"github.com/gogf/gf/v2/frame/g"
+	"github.com/gogf/gf/v2/os/gcron"
+)
+
+type (
+	Handler interface {
+		Name() string
+		Run(ctx context.Context) error
+	}
+
+	Config struct {
+		Name    string
+		Pattern string
+	}
+)
+
+var (
+	handlers map[string]Handler
+	rwMutex  sync.RWMutex
+	once     sync.Once
+)
+
+func SetHandler(handler Handler) {
+	name := handler.Name()
+	if name == "" {
+		panic("handler missing name")
+	}
+
+	rwMutex.Lock()
+	defer rwMutex.Unlock()
+
+	if handlers == nil {
+		handlers = map[string]Handler{}
+	}
+	if _, ok := handlers[name]; ok {
+		panic(fmt.Sprintf("handler %s already exists", name))
+	}
+	handlers[name] = handler
+}
+
+func Start(ctx context.Context) {
+	once.Do(func() {
+		configs := make([]*Config, 0)
+		if err := g.Config().MustGet(ctx, "crontab").Scan(&configs); err != nil {
+			panic(err)
+		}
+
+		rows := []string{"#", "NAME", "PATTERN", "HANDLER", "STATUS"}
+
+		table := tablewriter.NewTable(os.Stdout, tablewriter.WithConfig(tablewriter.Config{
+			Header: tw.CellConfig{
+				Merging: tw.CellMerging{Mode: tw.MergeBoth},
+			},
+			Row: tw.CellConfig{
+				Alignment: tw.CellAlignment{PerColumn: []tw.Align{tw.AlignCenter}},
+			},
+		}))
+		table.Header(garray.New().Pad(len(rows), "CRONTAB").Slice())
+		if err := table.Append(rows); err != nil {
+			panic(err)
+		}
+		defer func() {
+			if exception := recover(); exception != nil {
+				panic(exception)
+			}
+			if len(configs) > 0 {
+				_ = table.Render()
+			} else {
+				_ = table.Close()
+			}
+		}()
+
+		for index, config := range configs {
+			name := config.Name
+			pattern := config.Pattern
+			handler, ok := handlers[name]
+			if !ok {
+				panic(fmt.Sprintf("crontab %s not found", name))
+			}
+
+			if err := Runner(ctx, name, pattern, handler); err != nil {
+				panic(err)
+			}
+
+			if err := table.Append(index+1, name, pattern, fmt.Sprintf("%T", handler), "OK"); err != nil {
+				panic(err)
+			}
+		}
+	})
+}
+
+func Runner(ctx context.Context, name, pattern string, handler Handler) (err error) {
+	if _, err = gcron.AddSingleton(ctx, pattern, func(ctx context.Context) {
+		defer func() {
+			if exception := recover(); exception != nil {
+				g.Log().Error(ctx, exception)
+			}
+		}()
+
+		if err = handler.Run(ctx); err != nil {
+			g.Log().Infof(ctx, "crontab %s error: %s", name, err.Error())
+		} else {
+			g.Log().Infof(ctx, "crontab %s executed", name)
+		}
+	}, name); err != nil {
+		return
+	}
+
+	return
+}
