a4584c2f8f33310d3ad206ba8b29b9bb99a4e995
lowe21 <78665806@qq.com>
2026-08-10 17:21:59 +0800

    未配置DUBBO_GO_CONFIG_PATH时，使用本地config文件

--- core/config_center/config_center.go
+++ core/config_center/config_center.go
@@ -20,43 +20,56 @@ var once sync.Once
 
 func Init() {
 	once.Do(func() {
-		configAdapter := g.Config().GetAdapter().(*gcfg.AdapterFile)
-		configAdapter.SetFileName(genv.Get(constant.ConfigFileEnvKey, gcfg.DefaultConfigFileName).String())
+		dubboConfigFile := genv.Get(constant.ConfigFileEnvKey).String()
+		if dubboConfigFile != "" {
+			configAdapter := g.Config().GetAdapter().(*gcfg.AdapterFile)
+			configAdapter.SetFileName(dubboConfigFile)
 
-		centerConfig := &global.CenterConfig{}
-		if err := configAdapter.MustGet(nil, constant.ConfigCenterPrefix).Scan(centerConfig); err != nil {
-			panic(err)
-		}
+			centerConfig := &global.CenterConfig{}
+			if err := configAdapter.MustGet(nil, constant.ConfigCenterPrefix).Scan(centerConfig); err != nil {
+				panic(err)
+			}
 
-		if _, err := dubbo.NewInstance(
-			dubbo.WithConfigCenter(
-				config_center.WithConfigCenter(centerConfig.Protocol),
-				config_center.WithAddress(centerConfig.Address),
-				config_center.WithDataID(centerConfig.DataId),
-				config_center.WithCluster(centerConfig.Cluster),
-				config_center.WithGroup(centerConfig.Group),
-				config_center.WithUsername(centerConfig.Username),
-				config_center.WithPassword(centerConfig.Password),
-				config_center.WithNamespace(centerConfig.Namespace),
-				config_center.WithAppID(centerConfig.AppID),
-				config_center.WithTimeout(gconv.Duration(centerConfig.Timeout)),
-				config_center.WithParams(centerConfig.Params),
-				config_center.WithFileExtYaml(),
-			),
-		); err != nil {
-			panic(err)
-		}
+			if _, err := dubbo.NewInstance(
+				dubbo.WithConfigCenter(
+					config_center.WithConfigCenter(centerConfig.Protocol),
+					config_center.WithAddress(centerConfig.Address),
+					config_center.WithDataID(centerConfig.DataId),
+					config_center.WithCluster(centerConfig.Cluster),
+					config_center.WithGroup(centerConfig.Group),
+					config_center.WithUsername(centerConfig.Username),
+					config_center.WithPassword(centerConfig.Password),
+					config_center.WithNamespace(centerConfig.Namespace),
+					config_center.WithAppID(centerConfig.AppID),
+					config_center.WithTimeout(gconv.Duration(centerConfig.Timeout)),
+					config_center.WithParams(centerConfig.Params),
+					config_center.WithFileExtYaml(),
+				),
+			); err != nil {
+				panic(err)
+			}
 
-		properties, err := config.GetEnvInstance().GetDynamicConfiguration().GetProperties(centerConfig.DataId, config_center.WithGroup(centerConfig.Group))
-		if err != nil {
-			panic(err)
-		}
+			properties, err := config.GetEnvInstance().GetDynamicConfiguration().GetProperties(centerConfig.DataId, config_center.WithGroup(centerConfig.Group))
+			if err != nil {
+				panic(err)
+			}
 
-		configAdapter.SetContent(properties)
-		configAdapter.SetFileName(gcfg.DefaultConfigFileName)
+			configAdapter.SetContent(properties)
+			configAdapter.SetFileName(gcfg.DefaultConfigFileName)
 
-		if err = g.Server().SetConfigWithMap(configAdapter.MustGet(nil, "server").Map()); err != nil {
-			panic(err)
+			if err = g.Server().SetConfigWithMap(configAdapter.MustGet(nil, "server").Map()); err != nil {
+				panic(err)
+			}
+		} else {
+			configAdapter := g.Config().GetAdapter().(*gcfg.AdapterFile)
+			configFile, err := configAdapter.GetFilePath()
+			if err != nil {
+				panic(err)
+			}
+
+			if err = genv.Set(constant.ConfigFileEnvKey, configFile); err != nil {
+				panic(err)
+			}
 		}
 	})
 }
