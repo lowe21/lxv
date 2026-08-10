6b6fa3bab95c9c6630c25165337a450f91f1037f
lowe21 <78665806@qq.com>
2026-08-10 18:01:20 +0800

    未配置graylog则不启动远程日志中心

--- core/log_center/log_center.go
+++ core/log_center/log_center.go
@@ -21,43 +21,45 @@ func Init() {
 			panic(err)
 		}
 
-		logCenter := graylog.New()
+		if g.Config().MustGet(nil, "graylog.address").String() != "" {
+			logCenter := graylog.New()
 
-		g.Log().SetHandlers(func(ctx context.Context, input *glog.HandlerInput) {
-			input.Next(ctx)
+			g.Log().SetHandlers(func(ctx context.Context, input *glog.HandlerInput) {
+				input.Next(ctx)
 
-			content := input.ValuesContent()
-			delimiter := "\n"
-			stack := ""
+				content := input.ValuesContent()
+				delimiter := "\n"
+				stack := ""
 
-			for _, value := range input.Values {
-				strings := fmt.Sprintf("%+v", value)
-				if strings != "" {
-					array := gstr.Split(strings, delimiter)
-					stacks := make([]string, 0, len(array))
-					for _, item := range array {
-						if item != content {
-							stacks = append(stacks, item)
+				for _, value := range input.Values {
+					strings := fmt.Sprintf("%+v", value)
+					if strings != "" {
+						array := gstr.Split(strings, delimiter)
+						stacks := make([]string, 0, len(array))
+						for _, item := range array {
+							if item != content {
+								stacks = append(stacks, item)
+							}
+						}
+						if len(stacks) > 0 {
+							stack = gstr.Join([]string{stack, gstr.Join(stacks, delimiter)}, delimiter)
 						}
 					}
-					if len(stacks) > 0 {
-						stack = gstr.Join([]string{stack, gstr.Join(stacks, delimiter)}, delimiter)
-					}
 				}
-			}
 
-			if stack == "" {
-				stack = input.Stack
-			}
+				if stack == "" {
+					stack = input.Stack
+				}
 
-			logCenter.Send(ctx, &graylog.Gelf{
-				Host:         g.Server().GetName(),
-				ShortMessage: content,
-				FullMessage:  stack,
-				Timestamp:    gconv.Float64(input.Time.UnixMilli()) / 1e3,
-				Level:        input.Level,
-				LevelFormat:  input.LevelFormat,
+				logCenter.Send(ctx, &graylog.Gelf{
+					Host:         g.Server().GetName(),
+					ShortMessage: content,
+					FullMessage:  stack,
+					Timestamp:    gconv.Float64(input.Time.UnixMilli()) / 1e3,
+					Level:        input.Level,
+					LevelFormat:  input.LevelFormat,
+				})
 			})
-		})
+		}
 	})
 }
