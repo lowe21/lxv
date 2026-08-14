4a585901c45c7793003e6d965cd4ed62165357da
lowe21 <78665806@qq.com>
2026-08-14 16:10:03 +0800

    jwt payload 增加SessionKey参数

--- pkg/jwt/payload.go
+++ pkg/jwt/payload.go
@@ -8,6 +8,7 @@ type (
 	Payload struct {
 		TokenId    string `json:"tokenId"    valid:"required" description:"令牌ID" read-only:"true"`
 		IdentityId string `json:"identityId" valid:"required" description:"身份ID" read-only:"true"`
+		SessionKey string `json:"sessionKey" valid:"required" description:"会话密钥" read-only:"true"`
 	}
 
 	Claims struct {
