41fa728b9417c5d155f962f936a8ce015d542356
lowe21 <78665806@qq.com>
2026-08-19 09:27:20 +0800

    增加错误码

--- pkg/error_code/error_code.go
+++ pkg/error_code/error_code.go
@@ -10,12 +10,14 @@ var (
 	SystemBusy     = New("SYSTEM_BUSY", "系统繁忙")
 	InternalError  = New("INTERNAL_ERROR", "内部错误")
 	InvalidRequest = New("INVALID_REQUEST", "无效请求")
+	InvalidEvent   = New("INVALID_EVENT", "无效事件")
 	InvalidParam   = New("INVALID_PARAM", "参数错误")
 	InvalidSign    = New("INVALID_SIGN", "验签失败")
 	InvalidAuth    = New("INVALID_AUTH", "鉴权失败")
 	InvalidToken   = New("INVALID_TOKEN", "令牌失效")
 	BusinessError  = New("BUSINESS_ERROR", "业务异常")
 	OperationError = New("OPERATION_ERROR", "操作失败")
+	UnknownError   = New("UNKNOWN_ERROR", "未知错误")
 )
 
 type ErrorCode struct {
