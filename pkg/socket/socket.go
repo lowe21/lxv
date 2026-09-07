3c71ce978929735a627a7fb807eaf0b4abbfb688
lowe21 <78665806@qq.com>
2026-09-07 15:55:15 +0800

    改进socket start和stop方法

--- pkg/socket/socket.go
+++ pkg/socket/socket.go
@@ -20,25 +20,48 @@ type Socket struct {
 	register    *Register
 	connector   *Connector
 	broadcaster *Broadcaster
+	started     bool
 	ctx         context.Context
 	cancel      context.CancelFunc
-	once        sync.Once
+	mutex       sync.RWMutex
+	wg          sync.WaitGroup
 }
 
 func (s *Socket) Start() {
-	s.once.Do(func() {
-		s.ctx, s.cancel = context.WithCancel(context.Background())
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
-		if err := s.register.AddNode(s.ctx); err != nil {
-			g.Log().Errorf(s.ctx, "register node error, %v", err)
-		}
+	if s.started {
+		return
+	}
 
-		go s.register.Heartbeat(s.ctx)
-		go s.broadcaster.Subscribe(s.ctx)
-	})
+	s.ctx, s.cancel = context.WithCancel(context.Background())
+
+	if err := s.register.AddNode(s.ctx); err != nil {
+		g.Log().Errorf(s.ctx, "register node error, %v", err)
+	}
+
+	s.wg.Add(2)
+	go func() {
+		defer s.wg.Done()
+		s.register.Heartbeat(s.ctx)
+	}()
+	go func() {
+		defer s.wg.Done()
+		s.broadcaster.Subscribe(s.ctx)
+	}()
+
+	s.started = true
 }
 
 func (s *Socket) Connect(request *ghttp.Request, clientID string, group ...string) (err error) {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	if !s.started {
+		return errcode.New(errcode.ErrInvalidRequest, "socket is not started")
+	}
+
 	if clientID == "" {
 		return errcode.New(errcode.ErrInvalidRequest, "client id is empty")
 	}
@@ -72,13 +95,26 @@ func (s *Socket) Connect(request *ghttp.Request, clientID string, group ...strin
 }
 
 func (s *Socket) Stop() {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if s.started {
+		s.started = false
+	} else {
+		return
+	}
+
 	if s.cancel != nil {
 		s.cancel()
 	}
+
+	s.wg.Wait()
+
 	for _, group := range s.connector.GetGroups() {
 		for _, client := range s.connector.GetClients(group) {
 			client.Close([]byte("shutdown"))
 		}
 	}
+
 	_ = s.register.DeleteNode()
 }
